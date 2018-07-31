627df72167d339b42bcfde0f128c7a7e2e18638f
Kevin Zhang <40934037+kevinzhang237@users.noreply.github.com>
2018-07-31 16:24:20 -0700

    Scripts

--- App1/Assets/Scripts/Player/PlayerTorpedoController.cs
+++ App1/Assets/Scripts/Player/PlayerTorpedoController.cs
@@ -2,7 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerTorpedoController : MonoBehaviour {
+	public LayerMask torpedoMask;
+	public float maxDamage = 50f;
+	public float minDamage = 40f;
+	private float torpedoDamage;
+	public float timeToLive = 50f;
+	public float explosionRadius = 5f;
+	private bool activated = false;
+
+	public void Activate (){
+		Destroy (gameObject, timeToLive);
+		torpedoDamage = Random.Range (minDamage, maxDamage);
+		activated = true;
+	}
+
+	private void OnTriggerEnter(Collider other){
+		if (!activated) {
+			return;
+		}
+		Collider[] targets = Physics.OverlapSphere (transform.position, explosionRadius, torpedoMask);
+		for (int i = 0; i < targets.Length; i++) {
+			Rigidbody target = targets [i].GetComponent<Rigidbody> ();
+			if (!target){
+				continue;
+			}
+			torpedoImpact (target);
+			torpedoEffects ();
+		}
+		Destroy(gameObject);
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +42,17 @@ public class NewBehaviourScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	private void torpedoImpact(Rigidbody target){
+		DestructibleObject targetObject = target.GetComponent<DestructibleObject> ();
+		if (!targetObject) {
+			return;
+		}
+		targetObject.takeDamage (torpedoDamage);
+	}
+
+	private void torpedoEffects(){
+
+	}
+
 }
