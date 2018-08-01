030a9dc67cab98664f931fe6c1617f359f2eeae7
DelvinH <delvinhuang@yahoo.com>
2018-07-31 18:50:16 -0700

    edited PlayerTorpedoController and DestructibleObject for syntax

---
+++ App1/Assets/Scripts/PlayerTorpedo/DestructibleObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+	public float health;
+	public float maxHealth;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+	public float getHealth()
+    {
+		return health;
+	}
+
+	public void takeDamage(float amount)
+    {
+		if (amount <= 0)
+			return;
+		
+		health = Mathf.Max(0, health - amount);
+		if (health == 0)
+			objectBreak(); 
+	}
+
+	public void objectBreak()
+    {
+
+	}
+
+	public void healDamage(float amount)
+    {
+		if(amount <= 0)
+			return;
+		
+		health = Mathf.Min(maxHealth, health + amount);
+	}
+
+}
---
+++ App1/Assets/Scripts/PlayerTorpedo/PlayerTorpedoController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTorpedoController : MonoBehaviour
+{
+	public LayerMask torpedoMask;
+	public float maxDamage;
+	public float minDamage;
+	private float torpedoDamage;
+	public float maxLifetime;
+	public float explosionRadius;
+	private bool activated = false;
+
+	public void Activate ()
+    {
+		Destroy(gameObject, maxLifetime);
+		torpedoDamage = Random.Range(minDamage, maxDamage);
+		activated = true;
+	}
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+		if (!activated)
+			return;
+		
+		Collider[] targets = Physics.OverlapSphere(transform.position, explosionRadius, torpedoMask);
+		for (int i = 0; i < targets.Length; i++)
+        {
+			Rigidbody target = targets [i].GetComponent<Rigidbody>();
+			if (!target)
+				continue;
+			torpedoImpact(target);
+			torpedoEffects();
+		}
+		Destroy(gameObject);
+	}
+
+	private void torpedoImpact(Rigidbody target)
+    {
+		DestructibleObject targetObject = target.GetComponent<DestructibleObject>();
+		if (!targetObject) 
+			return;
+		targetObject.takeDamage(torpedoDamage);
+	}
+
+	private void torpedoEffects()
+    {
+
+	}
+
+}
